ac31c6846bb609c89f5c18918a1544b1f150b539
Alex Koehler <adjlchix@yahoo.com>
2022-03-02 08:06:09 -0500

    state machine/movement fixes

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -7,15 +7,23 @@ public class AI : MonoBehaviour{
 
 
     public float lookRadius = 10f;
+    public float hearRadius = 20f;
     Transform target;
     NavMeshAgent agent;
     private Vector3 startPosition;
+    [Range(1, 10)] public float walkRadius;
+    public bool waiting = false;
+    public float moveDelay = 5f;
+    public Vector3 centerSearch;
+
+    public enum State { Wander, Searching, Hunting }
 
     // Start is called before the first frame update
     void Start(){
         startPosition = transform.position;
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(RoamPosition());
         
     }
 
@@ -27,20 +35,36 @@ public class AI : MonoBehaviour{
         {
             agent.SetDestination(target.position);
         }
+        else if(distance <= hearRadius)
+        {
+            centerSearch = target.position;
+            agent.SetDestination(centerSearch);
+        }
         else
         {
+            if(agent != null && agent.remainingDistance <= agent.stoppingDistance)
             agent.SetDestination(RoamPosition());
+            waiting = true;
+            StartCoroutine(Wait());
         }
     }
 
     private Vector3 RoamPosition()
     {
-       return startPosition + GetRandomPosition() * Random.Range(10f, 30f);
+        Vector3 finalPosition = Vector3.zero;
+        Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
+        randomDirection += transform.position;
+        if(NavMesh.SamplePosition(randomDirection, out NavMeshHit hit, walkRadius, 1))
+        {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
     }
 
-    private Vector3 GetRandomPosition()
+    IEnumerator Wait()
     {
-        return new Vector3(UnityEngine.Random.Range(-2f, 2f), UnityEngine.Random.Range(-2f, 2f)).normalized;
+        yield return new WaitForSeconds(moveDelay);
+        waiting = false;
     }
 
     void OnDrawGizmosSelected(){
