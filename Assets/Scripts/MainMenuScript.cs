2424568b709b46cb9d41e02a281cbaea6a2abd00
shirnava <shirnava@gmail.com>
2022-03-07 18:50:38 -0500

    Added main menu scene and fonts

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public string firstLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(firstLevel);
+
+    }
+
+    public void openOptions()
+    {
+
+    }
+
+    public void closeOptions()
+    {
+        
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("quitting");
+    }
+}
