6ca310dda6d80683fb05e02281ab5de00be55d10
NYardich <65876107+NYardich@users.noreply.github.com>
2022-04-23 22:37:46 -0400

    adds lighting to school, adds some dialogue

--- Assets/Scripts/SceneEntrance.cs
+++ Assets/Scripts/SceneEntrance.cs
@@ -13,66 +13,9 @@ public class SceneEntrance : MonoBehaviour
     public bool libMarker;
     public GameObject player;
   
-    // IEnumerator waiter()
-    // {
-    //     currentScene = SceneManager.GetActiveScene().name;
-
-    //     // if((PlayerPrefs.GetString("LastExitName") == lastExitName) && (currentScene == "Town"))
-    //     // {
-    //     //     if(soccerThrowable == true)
-    //     //     {
-    //     //         GameObject.FindWithTag("TestThrowCube").transform.position = GameObject.FindWithTag("BallMarker").transform.position;
-    //     //         GameObject.FindWithTag("TestThrowCube").transform.eulerAngles = GameObject.FindWithTag("BallMarker").transform.eulerAngles;     
-    //     //     }
-    //     // }
-        
-    //     if(PlayerPrefs.GetString("LastExitName") == lastExitName)
-    //     {
-    //         GameObject.Find("GameManager").GetComponent<EnemyManager>().enemy = GameObject.FindWithTag("Enemy");
-    //         if (enemyMarker == true)
-    //         {
-    //                 Debug.Log("im in here");
-    //                 Debug.Log("the enemy transform position : " + GameObject.FindWithTag("EnemyMarker").transform.position);
-                  
-
-    //                 yield return new WaitForSeconds(1);
-    //                 GameObject.FindWithTag("Enemy").transform.position = GameObject.FindWithTag("EnemyMarker").transform.position;
-    //                 GameObject.FindWithTag("Enemy").transform.eulerAngles = GameObject.FindWithTag("EnemyMarker").transform.eulerAngles;
-                     
-    //         Debug.Log("the enemy player now position : " + GameObject.FindWithTag("Enemy").transform.position);
-    //         }
-    //         else
-    //         {
-    //             Debug.Log("the transform position : " + transform.position + " the transform is: " + transform);
-
-    //            GameObject.Find("PlayerCapsule").transform.position = transform.position; // if its in the x,y,z of the map? 
-    //             //GameObject.FindWithTag("CinemachineTarget").transform.position = transform.position;
-    //             GameObject.Find("PlayerCapsule").transform.eulerAngles = transform.eulerAngles;
-    //             //GameObject.FindWithTag("CinemachineTarget").transform.eulerAngles = transform.eulerAngles;
-
-    //             yield return new WaitForSeconds(2);
-       
-    //             Debug.Log("in trigger");
-    //             Debug.Log("The player is: " + GameObject.FindWithTag("Player"));
-
-    //            // GameObject.FindWithTag("Player").transform.position = transform.position;
-
-    //             Debug.Log("the player now position : " + GameObject.FindWithTag("Player").transform.position);
-
-    //             yield return new WaitForSeconds(1);
-
-    //             GameObject.FindWithTag("Enemy").transform.position = GameObject.FindWithTag("EnemyMarker").transform.position;
-    //             GameObject.FindWithTag("Enemy").transform.eulerAngles = GameObject.FindWithTag("EnemyMarker").transform.eulerAngles;      
-    //         }
-    //     }
-    // }
-    
-    void Start()
+    IEnumerator waiter()
     {
-        // player = GameObject.FindWithTag("Player");
-        
-        // StartCoroutine(waiter());
-         currentScene = SceneManager.GetActiveScene().name;
+        currentScene = SceneManager.GetActiveScene().name;
 
         // if((PlayerPrefs.GetString("LastExitName") == lastExitName) && (currentScene == "Town"))
         // {
@@ -92,7 +35,7 @@ public class SceneEntrance : MonoBehaviour
                     Debug.Log("the enemy transform position : " + GameObject.FindWithTag("EnemyMarker").transform.position);
                   
 
-                   // yield return new WaitForSeconds(1);
+                    yield return new WaitForSeconds(1);
                     GameObject.FindWithTag("Enemy").transform.position = GameObject.FindWithTag("EnemyMarker").transform.position;
                     GameObject.FindWithTag("Enemy").transform.eulerAngles = GameObject.FindWithTag("EnemyMarker").transform.eulerAngles;
                      
@@ -102,26 +45,34 @@ public class SceneEntrance : MonoBehaviour
             {
                 Debug.Log("the transform position : " + transform.position + " the transform is: " + transform);
 
-               GameObject.Find("PlayerCapsule").transform.position = transform.position; // if its in the x,y,z of the map? 
-                //GameObject.FindWithTag("CinemachineTarget").transform.position = transform.position;
-                GameObject.Find("PlayerCapsule").transform.eulerAngles = transform.eulerAngles;
-                //GameObject.FindWithTag("CinemachineTarget").transform.eulerAngles = transform.eulerAngles;
-
-              //  yield return new WaitForSeconds(2);
+                GameObject.FindWithTag("Player").transform.position = transform.position; // if its in the x,y,z of the map? 
+                
+                yield return new WaitForSeconds(0.001f);
        
                 Debug.Log("in trigger");
                 Debug.Log("The player is: " + GameObject.FindWithTag("Player"));
 
-               // GameObject.FindWithTag("Player").transform.position = transform.position;
+                GameObject.FindWithTag("Player").transform.position = transform.position;
 
                 Debug.Log("the player now position : " + GameObject.FindWithTag("Player").transform.position);
 
-//                yield return new WaitForSeconds(1);
+                GameObject.FindWithTag("CinemachineTarget").transform.position = transform.position;
+                GameObject.FindWithTag("Player").transform.eulerAngles = transform.eulerAngles;
+                GameObject.FindWithTag("CinemachineTarget").transform.eulerAngles = transform.eulerAngles;
+
+                yield return new WaitForSeconds(1);
 
                 GameObject.FindWithTag("Enemy").transform.position = GameObject.FindWithTag("EnemyMarker").transform.position;
                 GameObject.FindWithTag("Enemy").transform.eulerAngles = GameObject.FindWithTag("EnemyMarker").transform.eulerAngles;      
             }
         }
+    }
+    
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        
+        StartCoroutine(waiter());
     }
     
 
